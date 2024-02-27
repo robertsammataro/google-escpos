7f091234d047b2690c8173e228cd933ac7abcf0c
Robert Sammataro <robertsammataro@gmail.com>
2024-02-26 23:22:32 -0500

    Moved user prefs to pref.json

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import os
+import json
 from datetime import *
 from escpos import *
 from apiCaller import *
 
-# Prompts the user to enter their information which will be saved later.
-def onboard_user() -> None:
-    return
+global prefs
+
+def load_user_prefs(filepath='pref.json') -> dict:
+    """
+        Loads the user's preferences
+        Args:
+            filepath: str - Filepath to the user's preferences file. Defaults ot pref.json in
+                            the CWD but can also be manually supplied.
+    """
+    with open(filepath) as f:
+        data = json.load(f)
+    return data
 
 def test_printer(p: printer) -> None:
     """
@@ -18,7 +28,6 @@ def test_printer(p: printer) -> None:
     
 
 def p_date(p: printer, weekday=True, multiline=False) -> None:
-    
     """
         Prints the current date on the specified ESC/POS Device
         Args:
@@ -34,34 +43,47 @@ def p_date(p: printer, weekday=True, multiline=False) -> None:
         p.text(dt.strftime('\n'))
     p.textln(dt.strftime('%d %B %Y'))
 
-def generate_report() -> None:
-    #pp.pprint(retrieve_events())
+def p_calendar(p: printer, calendar_id) -> None:
+    """
+        Prints the specified calendar with formatting. Includes event name,
+        time, and location (if applicable)
+        Args:
+            p: printer       - escpos.printer the date will be printed to
+            calendar_id: str - calendar_id of the calendar that will be printed.
+    """
+    for item in retrieve_events(calendar_id=calendar_id, sort=True):
+        p.set(height=3, width=3, custom_size=True)
+        p.textln(item['summary'])
+        p.set(height=2, width=2, custom_size=True)
+        p.textln(to_local_tz(parse_result_to_time(item['start']['dateTime'])).strftime("%I:%M %p") + " - " + to_local_tz(parse_result_to_time(item['end']['dateTime'])).strftime("%I:%M %p"))
+        p.ln(5)
+        try:
+          p.text(item['location'])
+          p.ln(2)
+        except:
+          continue
+    
+    p.ln(100)
+
+def generate_report(p: printer, calendar_id: str='primary') -> None:
+    """
+        Generates a report of all scheduled events for the day and sends the report
+        to the printer.
+        Args:
+            p: printer       - escpos.printer the date will be printed to
+            calendar_id: str - calendar_id of the calendar that will be printed.
+    """
+    
     p = printer.Serial("COM4")
-    p.set(double_height=True, double_width=True)
-    p.set(height=3, width=3, custom_size=True)
+    p.set(double_height=True, double_width=True, height=3, width=3, custom_size=True)
     p.line_spacing(3)
-    p.text('Good Morning, Robby!\n')
+    p.text('Good Morning, ' + prefs['name'] + '!\n')
     p.set(height=2, width=2, custom_size=True)
     p.text("Today is ")
     p_date(p, multiline=False)
     p.ln(10)
     p.textln('Here\'s a look at your day:')
     p.ln(25)
-    
-    for item in retrieve_events(calendar_id='rsammataro1126@gmail.com', sort=True):
-      p.set(height=3, width=3, custom_size=True)
-      p.textln(item['summary'])
-      p.set(height=2, width=2, custom_size=True)
-      p.textln(to_local_tz(parse_result_to_time(item['start']['dateTime'])).strftime("%I:%M %p") + " - " + to_local_tz(parse_result_to_time(item['end']['dateTime'])).strftime("%I:%M %p"))
-      p.ln(5)
-      try:
-          p.text(item['location'])
-          p.ln(2)
-      except:
-          continue
-      
-    p.ln(100)
-    
+    p_calendar(p, calendar_id)  
 
-if __name__ == "__main__":
-    generate_report()
+prefs = load_user_prefs()
